4b3aa9bf3d3a5d1f371758cf79fa491220783de7
Yacine <simohammed.yacine@gmail.com>
2024-07-25 10:25:35 +0200

    changement de classe + redirection img

--- NK-Formation V2/src/formation/assets/js/main.js
+++ NK-Formation V2/src/formation/assets/js/main.js
@@ -1,29 +1,46 @@
-/*=============== SWIPER JS ===============*/
-let swiperCards = new Swiper(".card__content", {
-  loop: true,
-  spaceBetween: 32,
-  grabCursor: true,
+function initSwiper(containerSelector, paginationSelector, nextButtonSelector, prevButtonSelector, breakpoints) {
+  return new Swiper(containerSelector + " .card__content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints:{
-    600: {
-      slidesPerView: 2,
+    pagination: {
+      el: containerSelector + " " + paginationSelector,
+      clickable: true,
+      dynamicBullets: true,
     },
-    968: {
-      slidesPerView: 3,
+
+    navigation: {
+      nextEl: containerSelector + " " + nextButtonSelector,
+      prevEl: containerSelector + " " + prevButtonSelector,
     },
+
+    breakpoints: breakpoints,
+  });
+}
+
+// Définissez les breakpoints spécifiques pour chaque swiper
+const breakpointsSwiper1 = {
+  600: {
+    slidesPerView: 1,
   },
-});
+  991: {
+    slidesPerView: 2,
+  },
+};
+
+const breakpointsSwiper2 = {
+  600: {
+    slidesPerView: 2,
+  },
+  968: {
+    slidesPerView: 3,
+  }
+};
+
+// Initialisez chaque swiper avec des breakpoints différents
+let swiperCards1 = initSwiper(".swiper1", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev", breakpointsSwiper1);
+let swiperCards2 = initSwiper(".swiper2", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev", breakpointsSwiper2);
 
 
 
